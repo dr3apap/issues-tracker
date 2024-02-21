c6fe802b1658e0c3b4f4dd638a9f3c463f7dca63
Adebola Adesina <adebola@aadesina.com>
2024-02-21 14:49:21 -0800

    refactor:Refactor issue Type split application data from the issue data

--- src/Controllers/bugController.tsx
+++ src/Controllers/bugController.tsx
@@ -8,54 +8,54 @@ import MakeBugs from "../Models/bugsFactory";
 
 const bug1: MakeBugs = {
     _id: nanoid(),
-    name: "snake-game",
     issueType: "Freeze",
     description: "App suddenly froze and need to be restart",
     reporter: "Dre Michael",
-    timeStamp: new Date().toString().slice(0, 24),
+    timeReported: new Date().toString().slice(0, 24),
     priority: "High",
-    version: "V2.0",
-    issueState: "Open"
+    issueState: "Open",
+    issueTitle: "",
+    appName: "",
 
 
 }
 
 const bug2: MakeBugs = {
     _id: nanoid(),
-    name: "bug-tracker",
     issueType: "Crash",
     description: "App crash on startup",
     reporter: "Desmond Dessy",
-    timeStamp: new Date().toString().slice(0, 24),
+    timeReported: new Date().toString().slice(0, 24),
     priority: "High",
-    version: "V1.0",
-    issueState: "Open"
+    issueState: "Open",
+    issueTitle: "",
+    appName: "",
 }
 
 
 const bug3: MakeBugs = {
     _id: nanoid(),
-    name: "Daily-Planner",
     issueType: "Loading",
     description: "App stuck on loading",
     reporter: "Desiree Brat",
-    timeStamp: new Date().toString().slice(0, 24),
+    timeReported: new Date().toString().slice(0, 24),
     priority: "Medium",
-    version: "V3.0",
-    issueState: "In Progress"
+    issueState: "In Progress",
+    issueTitle: "",
+    appName: "",
 }
 
 
 const bug4: MakeBugs = {
     _id: nanoid(),
-    name: "Ticket-Master",
     issueType: "Crash",
     description: "App suddenly crash",
     reporter: "Sage Looney",
-    timeStamp: new Date().toString().slice(0, 24),
+    timeReported: new Date().toString().slice(0, 24),
     priority: "Low",
-    version: "V3.0",
-    issueState: "Closed"
+    issueState: "Closed",
+    issueTitle: "",
+    appName: "",
 }
 
 
@@ -63,15 +63,10 @@ const bug4: MakeBugs = {
 
 export default function getbugs() {
     const issues: MakeBugs[] = [];
-
-
     issues.push(new MakeBugs(bug1))
-
     issues.push(new MakeBugs(bug2))
     issues.push(new MakeBugs(bug3))
     issues.push(new MakeBugs(bug4))
-
-
     const sorted = issues.sort((a, b) => Number(a.priority) - Number(b.priority));
     return sorted;
 }
