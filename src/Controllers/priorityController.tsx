44cf1066594fe4af8d42dd8dfd64541f614dbbc7
Adebola Adesina <adebola@aadesina.com>
2024-02-21 14:51:07 -0800

    refactor:Refactor piority assigner

--- src/Controllers/priorityController.tsx
+++ src/Controllers/priorityController.tsx
@@ -1,35 +1,19 @@
-export interface PriorityOutput {
-    color: string;
-    level: string;
-}
+export type IssuePriority = string;
 
-export default function assignPrioriy(priority: string): PriorityOutput {
-    const colors: string[] = ["#FF6700", "#B33AA3", "#32CD32",];
+export default function assignPrioriy(priority: string): string {
+    let level = ""
     const levels: string[] = ["High", "Medium", "Low"];
+    if (priority == "Open".toLowerCase().trim()) {
+        level = levels[0]
+    } else if (priority == "In Progress".toLowerCase().trim()) {
+        level = levels[1]
+    } else if (priority == "Closed".toLowerCase().trim()) {
+        level = levels[2]
 
-    function mapArg(arg: string) {
-        if (arg == "Open") {
-            return {
-                color: colors[0],
-                level: levels[0]
-            }
-        } else if (arg == "In Progress") {
-            return {
-                color: colors[1],
-                level: levels[1]
-            }
-        } else if (arg == "Closed") {
-            return {
-                color: colors[2],
-                level: levels[2]
-            }
-        }
     }
-    const obj = mapArg(priority)
-    return obj as PriorityOutput
 
+    return level;
 }
-
 
 
 
