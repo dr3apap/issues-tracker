99ba4b2befb087999f576461b841835ccdfe82f8
Adebola Adesina <adebola@aadesina.com>
2024-02-21 15:24:48 -0800

    feature:Add issue lists component

--- src/Views/Components/Pages/BugsPriorityList.tsx
+++ src/Views/Components/Pages/BugsPriorityList.tsx
@@ -1,34 +1,39 @@
-import { useSelector } from 'react-redux'
+import { useAppSelector, useAppDispatch } from '../../../Controllers/Hooks/app-hooks';
 import { useParams, Link } from 'react-router-dom';
-import { RootState } from '../../../Controllers/Redux/rootReducer';
+import { AppDispatch } from '../../../Controllers/Redux/rootReducer';
 import MakeBugs from '../../../Models/bugsFactory';
+import { IssuesDispatch } from '../../../Controllers/Redux/bugSlice';
+import { reportBug, getClosedIssues, getBugs, updateBug, getOpenIssues, getInprogressIssues } from '../../../Controllers/Redux/bugSlice'
 import assignPriority from '../../../Controllers/priorityController';
 
 type PriorityType = "Open" | "In Progress" | "Closed"
 
+
 type BugsPriorityViewProps = {
-    bug: MakeBugs,
+    issue: MakeBugs,
 }
 
 export default function BugsPriorityList(/*props:BugsPriorityViewProps*/) {
-
-    const { issues } = useSelector((state: RootState) => state)
-    const { priority } = useParams()
-    const { level } = assignPriority(priority as PriorityType);
+    const { issueId } = useParams()
+    const d = `get${issueId}Issues`
+    console.log(`Inside BugsPriorityList:${IssuesDispatch["getOpenIssues"]}, d=${d},priorityId=${issueId}`)
+    const { issues } = useAppSelector((issues) => issues)
+    //const dispatch = useAppDispatch()
+    // const issue = dispatch(IssuesDispatch["getOpenIssues"]({ state: "Open" }))
+    const level = assignPriority(issueId as string);
 
     return (
         <div className="pageWrapper">
             {issues.length < 1 ? <h1>You have no bug to view</h1> : issues
-                .filter((bug) => Number(bug.priority) === Number(priority))
+                .filter((issue) => issue.priority === issueId)
                 .map((bug, key) => (
-                    <Link to={`/bugedit/${priority}`}>
+                    <Link to={`/bugedit/${issueId}`}>
                         <div key={key} className="bugDetail">
-                            <h1>{bug.name}</h1>
-                            <h4>{bug.timeStamp}</h4>
+                            <h4>{bug.timeReported}</h4>
                             <h4>{bug.issueType}</h4>
                             <p className="info">{bug.description}</p>
                             <h4>{level}</h4>
-                            <h4>{bug.bugStatus}</h4>
+                            <h4>{bug.issueState}</h4>
                             <h4>{bug.reporter}</h4>
                         </div>
                     </Link>
